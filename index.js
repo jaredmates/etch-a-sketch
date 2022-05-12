8eedba3e38c301931a77f2c35c0dc966f14300f8
Jared Mates <jaredmates45@gmail.com>
2022-05-11 20:04:06 -0700

    added button to change grid size

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const divContainer = document.querySelector(".grid-container");
+const button = document.querySelector(".button");
 
-for (let j = 0; j < 256; j++) {
-  let square = document.createElement("div");
-  square.classList.add("grid-square");
-  divContainer.appendChild(square);
-
-  square.addEventListener("mouseover", () => {
-    square.style.background = "#036bfc";
-  });
+function makeGrid(size) {
+  let divContainer = document.querySelector(".grid-container");
+  let squares = divContainer.querySelectorAll("div");
+  // Resets the grid
+  squares.forEach((div) => div.remove());
+  divContainer.style.setProperty("--grid-size", size);
+  for (let j = 0; j < size ** 2; j++) {
+    let square = document.createElement("div");
+    square.classList.add("grid-square");
+    divContainer.appendChild(square);
+    // Hover Effect
+    square.addEventListener("mouseover", () => {
+      square.style.background = "#036bfc";
+    });
+  }
 }
+
+button.addEventListener("click", () => {
+  let numSquares = prompt("Please enter number of squares per side:");
+  if (numSquares >= 2 && numSquares <= 100) {
+    makeGrid(numSquares);
+  } else {
+    console.log("Invalid Input! Too many squares!");
+  }
+});
 
 /* Alternative Method to making the Grid using Float property */
 // const divContainer = document.querySelector(".grid-container");
